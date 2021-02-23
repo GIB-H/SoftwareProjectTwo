60a018824b413957e2bf7dbcf6d4ce2e3fa109bf
George <72026233+GIB-H@users.noreply.github.com>
2021-02-23 15:50:23 +0000

    Added a login method to Main, currently blank and requires filling in

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+    }
 
+    public static void logIn(){
+        /* PULLS DATA FROM A DATABASE INTO VALUES BELOW
+        String accountUsername;
+        String firstName;
+        String lastName;
+        String emailAddress;
+        int accountBalance;
+        int privilegeLevel;
+        // TO BE PULLED IN FROM DATABASE
+
+        // CHECKS PASSWORD IS VALID
+
+        // CHECKS THE USERS PRIVILEGE LEVEL
+
+        // CALLS CONSTRUCTOR DEPENDING ON USER PRIVILEGE LEVEL
+        */
     }
 }
