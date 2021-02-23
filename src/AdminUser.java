24f956b706a085cf3f0172b53805d2486c5e7b08
George <72026233+GIB-H@users.noreply.github.com>
2021-02-23 15:42:13 +0000

    Added Account Superclass and blank User, AdminUser and PremiumUser subclasses

---
+++ src/AdminUser.java
@@ -0,0 +1,5 @@
+public class AdminUser extends Account{
+    AdminUser(String accountUsername, String firstName,String lastName, String emailAddress, int accountBalance, int privilegeLevel){
+        super(accountUsername,firstName,lastName,emailAddress,accountBalance,privilegeLevel);
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,5 @@
+public class User extends Account{
+    User(String accountUsername, String firstName,String lastName, String emailAddress, int accountBalance, int privilegeLevel){
+        super(accountUsername,firstName,lastName,emailAddress,accountBalance,privilegeLevel);
+    }
+}
