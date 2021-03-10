5912e9d773edbf99dfbac5b6c346d140f4696ff4
George <72026233+GIB-H@users.noreply.github.com>
2021-03-10 16:54:59 +0000

    Created the Encryption package and the Encrypt Class to handle information encryption.

---
+++ src/Encryption/Encrypt.java
@@ -0,0 +1,31 @@
+package Encryption;
+
+public class Encrypt {
+
+    // Creates Encryption Instance Variables
+    int encryptionKey = 12;
+
+    public String encryptData(String plaintextString){
+        String container = "";
+
+        char[] chars = plaintextString.toCharArray();
+
+        for(char character : chars) {
+            character += encryptionKey;
+            container += character;
+        }
+        return container;
+    }
+
+    public String decryptData(String encryptedString){
+        String container = "";
+
+        char[] chars = encryptedString.toCharArray();
+
+        for(char character : chars){
+            character -= encryptionKey;
+            container += character;
+        }
+        return container;
+    }
+}
